35754cb1016b308c0c0c5f319e4f1c64239df4f4
haider <haider.shah@hotmail.com>
2019-09-10 10:56:06 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -21,7 +21,7 @@ end
 def apply_coupons(cart, coupons)
   coupons.each do |coupon|
     item= coupon[:item]
-    if cart[item] && coupon[:item][:count]>=coupon[:num] && cart["#{}"]
+    if cart[item] && coupon[:item][:count]>=coupon[:num] && !cart["#{item} W/COUPON"]
     binding.pry  
 end
 end
