222f88404120640c1afd95263a1135376265739b
haider <haider.shah@hotmail.com>
2019-09-10 11:24:37 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -33,3 +33,28 @@ def apply_coupons(cart, coupons)
 end
 cart
 end
+
+
+def apply_clearance(cart)
+  cart.each do |item, price_hash|
+    if price_hash[:clearance] == true
+      price_hash[:price] = (price_hash[:price] * 0.8).round(2)
+    end
+  end
+  cart
+end
+
+def checkout(items, coupons)
+  cart = consolidate_cart(items)
+  cart1 = apply_coupons(cart, coupons)
+  cart2 = apply_clearance(cart1)
+  
+  total = 0
+  
+  cart2.each do |name, price_hash|
+    total += price_hash[:price] * price_hash[:count]
+  end
+  
+  total > 100 ? total * 0.9 : total
+  
+end
